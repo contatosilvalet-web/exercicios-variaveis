be2ec8165e79c02aa8207387dc908f6a29252734
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-09 16:18:54 -0300

    Update index.js

---
+++ src/05/index.js
@@ -0,0 +1,8 @@
+const raio = 1;
+
+const comprimento = 2 * Math.PI * raio;
+
+const area = Math.PI * (raio ** 2);
+
+console.log("Comprimento do círculo: " + comprimento.toFixed(1));
+console.log("Área do círculo: " + area.toFixed(2));
