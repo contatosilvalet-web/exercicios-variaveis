d79730f7809ce2234bfacf4713d78fc818434a29
Leticia Pedro <contato.silvalet@gmail.com>
2026-07-09 16:15:33 -0300

    Update index.js

---
+++ src/04/index.js
@@ -0,0 +1,8 @@
+const x1 = 2;
+const x2 = 4;
+const y1 = 8;
+const y2 = 16;
+
+const distancia = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+console.log("A distância entre os pontos é: " + distancia.toFixed(0));
